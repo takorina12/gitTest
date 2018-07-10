049ab418f1ec36370a1b779c690a843c70af6d63
kosmo-02 <kosmo-02@kosmo-02-PC>
2018-07-10 12:12:20 +0900

    BTest 두번째 커밋 : A사용자

--- JAVA_Git/src/BTest.java
+++ JAVA_Git/src/BTest.java
@@ -2,6 +2,8 @@
 public class BTest {
 	public static void main(String [] arge) {
 		
-		System.out.println("BTest ù Ŀ��");
+		System.out.println("BTest 첫 커밋");
+		System.out.println("BTest 두번째 커밋 : A사용자 ");
+
 	}
 }
